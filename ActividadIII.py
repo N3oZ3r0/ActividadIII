febd873d34db837bbde715ab25cbabef971dcf74
N3oZ3r0 <apocaliptico@gmail.com>
2020-04-14 23:24:30 +0200

    Update ActividadIII.py

--- ActividadIII.py
+++ ActividadIII.py
@@ -1,42 +1,59 @@
 import math
-import multiprocessing as mp  # Para trabajar en paralelo
-
+import multiprocessing as mp
 import numpy as np
 
+#Funcion merge que ordena el array en su totalidad
+def merge(*args):
+    izq, der = args[0] if len(args) == 1 else args
+    izq_length, der_length = len(izq), len(der)
+    izq_index, der_index = 0, 0
+    merged = []
+    while izq_index < izq_length and der_index < der_length:
+        if izq[izq_index] <= der[der_index]:
+            merged.append(izq[izq_index])
+            izq_index += 1
+        else:
+            merged.append(der[der_index])
+            der_index += 1
+    if izq_index == izq_length:
+        merged.extend(der[der_index:])
+    else:
+        merged.extend(izq[izq_index:])
+    return merged
+
+#Funcion merge que ordena cada trozo del array
+def merge_sort(X):
+    tam = len(X)
+
+    if tam <= 1:
+        return X
+
+    middle = tam // 2
+    izq = merge_sort(X[:middle])
+    der = merge_sort(X[middle:])
+
+    return merge(izq, der)
 
 def Orden(X):
-    n_cores = mp.cpu_count()
-    print(n_cores)
-    dimension = len(X)  # Obtengo num de elementos en el array
-    seccion = math.ceil(dimension / n_cores)
-    print(seccion)
-    MC = mp.RawArray('i', dimension)  # Array MC de memoria compartida donde se almacenaran
-    cores = []  # Array para guardar los cores y su trabajo
-    for core in range(n_cores):  # Asigno a cada core el trabajo que le toca, ver excel adjunto
-        i_MC = min(core * seccion,
-                   dimension)  # Calculo i para marcar inicio del trabajo del core en relacion a las filas
-        f_MC = min((core + 1) * seccion, dimension)  # Calculo f para marcar fin del trabajo del core, ver excel
-        cores.append(mp.Process(target=par_core, args=(X, MC, i_MC, f_MC)))  # Añado al Array los cores y su trabajo
-    for core in cores:
-        core.start()  # Arranco y ejecuto el trabajo para c/ uno de los cores que tenga mi equipo, ver excel
-    for core in cores:
-        core.join()  # Bloqueo cualquier llamada hasta que terminen su trabajo todos los cores
-    Y = [[0] * dimension for i in range(dimension)]  # Convierto el array unidimensional MC en una matrix 2D (C_2D)
-    for i in range(dimension):  # i para iterar sobre las filas de A
-        Y[i] = MC[i]  # Guardo el C_2D los datos del array MC
-    return Y
+    cores = mp.cpu_count()
+    seccion = math.ceil(len(X) / cores)
+    pool = mp.Pool(processes=cores)
+    X = [X[i * seccion:(i + 1) * seccion] for i in range(cores)]
+    X = pool.map(merge_sort, X)
 
+    while len(X) > 1:
+        extra = X.pop() if len(X) % 2 == 1 else None
+        X = [(X[i], X[i + 1]) for i in range(0, len(X), 2)]
+        X = pool.map(merge, X) + ([extra] if extra else [])
 
-def par_core(X, MC, i_MC, f_MC):  # La tarea que hacen todos los cores
-    temp = [len(X)]
-    for i in range(i_MC, f_MC):  # Size representado en colores en el excel que itera sobre las filas en A
-        temp.append(X[i])
-    temp.sort()
-    MC.append(temp)
+    return X[0]
 
 
 if __name__ == '__main__':
-    n = 24
-    X = np.random.randint(1, 5, n)  # Genero A[21535220][6]con num. aleatorios del 0 al 215, ver excel
+    n = 218060108
+    X = np.random.randint(1, 256, n)  # Genero un array de tamaño n con valores aleatorios del 1 al 256
+
     print('\n\nEl Array original es asi\n\n', X)
+
+
     print('\n\nEl Array ordenado ha quedado asi\n\n', Orden(X))
